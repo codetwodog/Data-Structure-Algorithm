918db231c1389628a15d0aa41bbd57966528d6f6
Jinhao Qi <jinhao.qi@outlook.com>
2022-09-08 10:32:24 +0800

    [other] - delete Test.java file

--- xin/twodog/DataStructureAndAlgorithm/Test.java
+++
@@ -1,9 +0,0 @@
-package xin.twodog.DataStructureAndAlgorithm;
-
-public class Test {
-    public static void main(String[] args) {
-        RomanToInteger romanToInteger = new RomanToInteger();
-        int mcmxciv = romanToInteger.romanToInt("MCMXCIV");
-        System.out.println(mcmxciv);
-    }
-}
